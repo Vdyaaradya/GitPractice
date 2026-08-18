82a6a6836500465de362367326ecb491b14a04ce
Vidyasree KJ <Vidyasree KJ@FFE-Vidyasree-KJ>
2026-08-18 12:18:16 +0530

    added one line logic in random class

--- GitPractice/src/test/java/testGit/random.java
+++ GitPractice/src/test/java/testGit/random.java
@@ -5,6 +5,7 @@ public class random {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hiiiii12333ii");
+System.out.println("hiiiii12333ii");
 	}
 
 }
