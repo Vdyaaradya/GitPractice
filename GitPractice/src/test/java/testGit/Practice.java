da1b3557e8377e35a1b9ab8ce26043f371b0d5b1
Vidyasree KJ <Vidyasree KJ@FFE-Vidyasree-KJ>
2026-08-18 13:09:18 +0530

    neeww

--- GitPractice/src/test/java/testGit/Practice.java
+++ GitPractice/src/test/java/testGit/Practice.java
@@ -10,6 +10,7 @@ public class Practice {
 		System.out.println("123456543234565432356");
 		System.out.println("Helloooooooo34565432356");
 		System.out.println("123456543234565432356pandaaa");
+		System.out.println("123456543234565432356pandaaa");
 	}
 
 }
